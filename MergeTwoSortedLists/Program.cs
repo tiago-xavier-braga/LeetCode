3eb20d1843718b1ef9c5868793b78789ce49206e
Tiago Xavier Braga <braga.tiagoxavier@gmail.com>
2024-03-24 15:01:47 -0300

    feat-code: add script that will send test values

---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.MergeTwoSortedLists
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ListNode indexATree = new(3);
+            ListNode indexATwo = new(2, indexATree);
+            ListNode indexAOne = new(1, indexATwo);
+
+            ListNode indexBTree = new(4);
+            ListNode indexBTwo = new(3, indexBTree);
+            ListNode indexBOne = new(1, indexBTwo);
+
+            List<ListNode> listA = new()
+            {
+                indexAOne,
+                indexATwo,
+                indexATree
+            };
+
+            List<ListNode> listB = new()
+            {
+                indexBOne,
+                indexBTwo,
+                indexBTree
+            };
+
+            List<List<ListNode>> listNodes = new() {listA, listB};
+            int loopCount = listNodes.Max(list => list.Count());
+
+            Solution solution = new();
+            for (int currentIndex = 0; currentIndex < loopCount; currentIndex++)
+            {
+                solution.MergeTwoLists(listA[currentIndex], listB[currentIndex]);
+            }
+
+        }
+    }
+}
---
+++ MergeTwoSortedLists/Solution/Solution.cs
@@ -0,0 +1,10 @@
+namespace LeetCode.MergeTwoSortedLists
+{
+    public class Solution {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+            
+            Console.WriteLine($"{list1.val} + {list1.next}");
+            return list1; 
+        }
+    }
+}
